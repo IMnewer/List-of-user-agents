33e31100bfd6814077ce662b2fd0e0d94c5d748c
nu11secur1ty <systadmi@abv.bg>
2019-06-10 22:59:46 +0300

    add ua.py-parser

--- ua.py
+++ ua.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python3
 import urllib.request
 
-fp = urllib.request.urlopen("https://raw.githubusercontent.com/nu11secur1ty/List-of-user-agents/master/Chrome.txt")
-mybytes = fp.read()
+row = urllib.request.urlopen("https://raw.githubusercontent.com/nu11secur1ty/List-of-user-agents/master/Chrome.txt")
+mylists = row.read()
 
-mystr = mybytes.decode("utf8")
-fp.close()
+mystr = mylists.decode("utf8")
+row.close()
 
 print(mystr)
