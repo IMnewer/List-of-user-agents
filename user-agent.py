700dbca78b675ee81666e6360444563584846bcc
nu11secur1ty <venvaropt@gmail.com>
2019-06-10 23:01:49 +0300

    Update user-agent.py

--- user-agent.py
+++ user-agent.py
@@ -2,8 +2,7 @@ import requests as req
 from bs4 import BeautifulSoup
 import time
 
-#url = 'http://www.useragentstring.com/pages/useragentstring.php?name=Firefox'
-url = 'https://github.com/cvandeplas/pystemon/blob/master/user-agents.txt'
+url = 'http://www.useragentstring.com/pages/useragentstring.php?name=Firefox'
 
 def save(br,ua):
 	
