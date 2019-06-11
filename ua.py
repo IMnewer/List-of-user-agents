8b8deb8a9fddffbfc33ce155b3e094dc0c2c4c52
nu11secur1ty <venvaropt@gmail.com>
2019-06-11 11:08:45 +0300

    Update UA

--- ua.py
+++ ua.py
@@ -8,3 +8,12 @@ mystr = mylists.decode("utf8")
 row.close()
 
 print(mystr)
+
+rowb = urllib.request.urlopen("https://raw.githubusercontent.com/nu11secur1ty/List-of-user-agents/master/Firefox.txt")
+mylistsb = rowb.read()
+
+mystrb = mylistsb.decode("utf8")
+rowb.close()
+
+print(mystrb)
+
